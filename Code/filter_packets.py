f8011c47741596d5b23ff056033bbcd34cc6899b
Peter Mastropaolo <petermastropaolo@yahoo.com>
2021-11-16 17:09:45 -0500

    Commented Filter and Parser

--- Code/filter_packets.py
+++ Code/filter_packets.py
@@ -1,1 +1,3 @@
 #Peter Mastropaolo (pjm8331)
+
+#Only adds lines that include ICMP and Echo in them to the new NodeX_filtered.txt file
--- Code/packet_parser.py
+++ Code/packet_parser.py
@@ -1,1 +1,4 @@
 #Peter Mastropaolo (pjm8331)
+
+#Returns a list of lists
+#Each line of the filtered file is split into a list by the delimiter (' ') and added to the master list
