a93ec4f2d47f293e09f9f3982a6409fc35f88709
Peter Mastropaolo <petermastropaolo@yahoo.com>
2021-11-16 15:32:30 -0500

    Packet Analyzer Comment Added

--- Code/packet_analyzer.py
+++ Code/packet_analyzer.py
@@ -1,1 +1,3 @@
 #Peter Mastropaolo (pjm8331)
+
+#Expected Return from compute is a list = [totReqSent, totReqRecv, totRepSent, totRepRecv, totReqBytesSent, totReqDataSent, totReqBytesRecv, totReqDataRecv, avgRTT, eput, gput, avgReplyDelay, avgEchoHop]
