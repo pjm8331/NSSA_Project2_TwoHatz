c8b7f70f8c402b10ebccc447fe3624df3071ea38
Teo Luciani <lucianiteo7@gmail.com>
2021-11-17 17:41:26 -0500

    initial filter built

--- Code/filter_packets.py
+++ Code/filter_packets.py
@@ -1,3 +1,24 @@
 #Peter Mastropaolo (pjm8331)
 
 #Only adds lines that include ICMP and Echo in them to the new NodeX_filtered.txt file
+
+def filter(filename):
+
+
+	f = open(filename, 'r')
+	line = f.readline().strip()
+
+	newfn = filename.strip(".txt")  + "_filtered" 
+
+	fw = open(newfn, 'w')
+
+	for line in f.readlines():
+		if "ICMP" in line:
+			fw.write(line)
+
+	f.close()
+	fw.close()
+
+#test script
+
+filter("example.txt")
