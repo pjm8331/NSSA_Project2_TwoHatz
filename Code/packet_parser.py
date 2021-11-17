b751a0b0fd00ade01f71707ac1947d0178dc5608
Teo Luciani <lucianiteo7@gmail.com>
2021-11-17 18:02:41 -0500

    parser starter

--- Code/packet_parser.py
+++ Code/packet_parser.py
@@ -1,4 +1,18 @@
 #Peter Mastropaolo (pjm8331)
-
+#Teo Luciani (ttl5052)
 #Returns a list of lists
 #Each line of the filtered file is split into a list by the delimiter (' ') and added to the master list
+
+def parse(filename):
+	
+	packetlist = []
+
+	f = open(filename, 'r')
+
+	for line in f.readlines():
+		listline = line.split()
+		packetlist.append(listline)
+
+	f.close()
+	return packetlist
+
