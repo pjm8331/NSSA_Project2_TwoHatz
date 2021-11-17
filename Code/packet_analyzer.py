e07acdb382e4e1c877e8e3abf22c5a504d317d80
Joshua Kelly <jmk3378@rit.edu>
2021-11-17 17:58:38 -0500

    Corrected Received typo.

--- Code/packet_analyzer.py
+++ Code/packet_analyzer.py
@@ -45,12 +45,12 @@ node4_data = foo("192.168.200.2", node4_parsed)
  f = open("project2_output.txt", "w")
 
 f.write("Node 1 \n")
-f.write("Echo Requests Sent\tEcho Requests Recieved\tEcho Replies Sent\tEcho Replies Received")
+f.write("Echo Requests Sent\tEcho Requests Received\tEcho Replies Sent\tEcho Replies Received")
 f.write(node1_data[0] + "\t" + node1_data[1] + "\t" + node1_data[2] + node1_data[3] +"\n")
 f.write("Echo Request Bytes Sent (bytes)/tEcho Request Data Sent (bytes)")
 f.write(node1_data[4] + "\t" + node1_data[5] + "\n")
 
-f.write("Echo Request Bytes Recieved (bytes)/tEcho Request Data Recieved (bytes)")
+f.write("Echo Request Bytes Received (bytes)/tEcho Request Data Received (bytes)")
 f.write(node1_data[6] + "\t" + node1_data[7] + "\n")
 f.write("\nAverage RTT (miliseconds) " + node1_data[8])
 f.write("Echo Request Throughput (kB/sec) " + node1_data[9])#input num here
@@ -60,12 +60,12 @@ f.write("Average Echo Request Hop Count " + node1_data[12])#input num here
 f.write("\n")
 
 f.write("Node 2\n")
-f.write("Echo Requests Sent\tEcho Requests Recieved\tEcho Replies Sent\tEcho Replies Received")
+f.write("Echo Requests Sent\tEcho Requests Received\tEcho Replies Sent\tEcho Replies Received")
 f.write(node2_data[0] + "\t" + node2_data[1] + "\t" + node2_data[2] + node2_data[3] +"\n")
 f.write("Echo Request Bytes Sent (bytes)/tEcho Request Data Sent (bytes)")
 f.write(node2_data[4] + "\t" + node2_data[5] + "\n")
 
-f.write("Echo Request Bytes Recieved (bytes)/tEcho Request Data Recieved (bytes)")
+f.write("Echo Request Bytes Received (bytes)/tEcho Request Data Received (bytes)")
 f.write(node2_data[6] + "\t" + node2_data[7] + "\n")
 f.write("\nAverage RTT (miliseconds) " + node2_data[8])
 f.write("Echo Request Throughput (kB/sec) " + node2_data[9])#input num here
@@ -75,12 +75,12 @@ f.write("Average Echo Request Hop Count " + node2_data[12])#input num here
 f.write("\n")
 
 f.write("Node 3\n")
-f.write("Echo Requests Sent\tEcho Requests Recieved\tEcho Replies Sent\tEcho Replies Received")
+f.write("Echo Requests Sent\tEcho Requests Received\tEcho Replies Sent\tEcho Replies Received")
 f.write(node3_data[0] + "\t" + node3_data[1] + "\t" + node3_data[2] + node3_data[3] +"\n")
 f.write("Echo Request Bytes Sent (bytes)/tEcho Request Data Sent (bytes)")
 f.write(node3_data[4] + "\t" + node3_data[5] + "\n")
 
-f.write("Echo Request Bytes Recieved (bytes)/tEcho Request Data Recieved (bytes)")
+f.write("Echo Request Bytes Received (bytes)/tEcho Request Data Received (bytes)")
 f.write(node3_data[6] + "\t" + node3_data[7] + "\n")
 f.write("\nAverage RTT (miliseconds) " + node3_data[8])
 f.write("Echo Request Throughput (kB/sec) " + node3_data[9])#input num here
@@ -90,12 +90,12 @@ f.write("Average Echo Request Hop Count " + node3_data[12])#input num here
 f.write("\n")
 
 f.write("Node 4\n")
-f.write("Echo Requests Sent\tEcho Requests Recieved\tEcho Replies Sent\tEcho Replies Received")
+f.write("Echo Requests Sent\tEcho Requests Received\tEcho Replies Sent\tEcho Replies Received")
 f.write(node4_data[0] + "\t" + node4_data[1] + "\t" + node4_data[2] + node4_data[3] +"\n")
 f.write("Echo Request Bytes Sent (bytes)/tEcho Request Data Sent (bytes)")
 f.write(node4_data[4] + "\t" + node4_data[5] + "\n")
 
-f.write("Echo Request Bytes Recieved (bytes)/tEcho Request Data Recieved (bytes)")
+f.write("Echo Request Bytes Received (bytes)/tEcho Request Data Received (bytes)")
 f.write(node4_data[6] + "\t" + node4_data[7] + "\n")
 f.write("\nAverage RTT (miliseconds) " + node4_data[8])
 f.write("Echo Request Throughput (kB/sec) " + node4_data[9])#input num here
